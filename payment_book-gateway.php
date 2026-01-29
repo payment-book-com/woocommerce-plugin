79303873f1320cd4ab98faa03a26a8454ed787ad
microwave <maksimsge@gmail.com>
2026-01-29 18:16:24 +0200

    Initial structure

--- payment_book-gateway.php
+++ payment_book-gateway.php
@@ -14,7 +14,7 @@ add_action( 'plugins_loaded', function() {
 
     if ( ! class_exists( 'WC_Payment_Gateway' ) ) return;
 
-    require_once __DIR__ . '/includes/class-payment-book-gateway.php';
+    require_once __DIR__ . '/includes/payment_book-gateway.php';
 
     add_filter( 'woocommerce_payment_gateways', function( $gateways ) {
         $gateways[] = 'PBGateway\\PaymentBook_Gateway';
