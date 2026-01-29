8e7f66e96bca22309726bb38f61c22016f695327
microwave <maksimsge@gmail.com>
2026-01-29 17:36:31 +0200

    Initial structure

---
+++ payment_book-gateway.php
@@ -0,0 +1,23 @@
+<?php
+/*
+Plugin Name: PAYMENT BOOK Gateway
+Description: WooCommerce payment gateway for PAYMENT BOOK
+Version: 0.0.1
+Author: DOMONK Factory
+*/
+
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+require __DIR__ . '/vendor/autoload.php';
+
+add_action( 'plugins_loaded', function() {
+
+    if ( ! class_exists( 'WC_Payment_Gateway' ) ) return;
+
+    require_once __DIR__ . '/includes/class-wltpay-gateway.php';
+
+    add_filter( 'woocommerce_payment_gateways', function( $gateways ) {
+        $gateways[] = 'WltPayGateway\\WltPay_Gateway';
+        return $gateways;
+    });
+});
